12fe4a39fb3cc6a3335bb15897e1f5085182ba21
ramywthyq-byte <ramywthyq@gmail.com>
2026-06-26 02:48:48 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,20 +1,34 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $urltiktok = "https://vt.tiktok.com/ZSC2EShTm/";
 $apikey = "74afaadd7ffa8a725bd2586afa22ba17";
 
-// ** غيّر هذا السطر فقط في كل تجربة **
 $apiurl = "https://api.tikwm.com/?" . http_build_query([
     'url'   => $urltiktok,
     'hd'    => 1,
-    'token' => $apikey   // جرب: token أو key أو api_key
+    'token' => $apikey
 ]);
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $apiurl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
 $response = curl_exec($ch);
+$curl_error = curl_error($ch);
 curl_close($ch);
+
+// اطبع الخطأ إذا في
+if ($curl_error) {
+    echo "cURL Error: " . $curl_error;
+    exit;
+}
+
+// اطبع الرد الخام قبل التحويل
+echo "Raw Response: " . $response . "<br><br>";
 
 $data = json_decode($response, true);
 echo "<pre>";
