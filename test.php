45041a3f09926a7176783bee5c53e38944fe5a9e
ramywthyq-byte <ramywthyq@gmail.com>
2026-06-26 02:27:20 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,10 @@
 <?php
+
 $urltiktok = "https://vt.tiktok.com/ZSC2EShTm/";
 $apikey = "74afaadd7ffa8a725bd2586afa22ba17";
 
-
-// رابط الـ API
-$apiurl = "https://api.tikwmapi.com/?url=" . urlencode($urltiktok) . "&hd=1;
+// رابط API
+$apiurl = "https://api.tikwmapi.com/?url=" . urlencode($urltiktok) . "&hd=1";
 
 // تهيئة cURL
 $ch = curl_init();
@@ -12,28 +12,21 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $apiurl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-// 🔥 هنا نرسل المفتاح في الهيدر
+// إرسال API Key في الهيدر
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     "x-api-key: " . $apikey
 ]);
 
-$response = curl_exec($ch);
-
-curl_close($ch);
-
-// إرسال الطلب
+// تنفيذ الطلب
 $response = curl_exec($ch);
 
 // إغلاق الاتصال
 curl_close($ch);
 
-// تحويل الرد من JSON إلى PHP Array
+// تحويل JSON إلى Array
 $data = json_decode($response, true);
 
-// عرض النتيجة (للتجربة فقط)
+// عرض النتيجة
 echo "<pre>";
 print_r($data);
 echo "</pre>";
-echo $apikey;
-exit;
-?>
