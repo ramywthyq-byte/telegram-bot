e0c8a26717a8293788a33931763e01ecd0939ecc
ramywthyq-byte <ramywthyq@gmail.com>
2026-06-27 02:47:33 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,35 +1,33 @@
 <?php
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $urltiktok = "https://vt.tiktok.com/ZSC2EShTm/";
-$apikey = "7f66b6ac908d5c3b266c30c62f87b9c5";
 
-$apiurl = "https://api.tikwmapi.com/?" . http_build_query([
-    'url'   => $urltiktok,
-    'hd'    => 1,
-    'api_key' => $apikey
+$apiurl = "https://www.tikwm.com/api/?" . http_build_query([
+    'url' => $urltiktok,
+    'hd'  => 1
 ]);
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $apiurl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 $response = curl_exec($ch);
-$curl_error = curl_error($ch);
-curl_close($ch);
 
-if ($curl_error) {
-    echo "cURL Error: " . $curl_error;
+if (curl_errno($ch)) {
+    echo "cURL Error: " . curl_error($ch);
     exit;
 }
 
-echo "Raw Response: " . $response . "<br><br>";
+curl_close($ch);
 
-$data = json_decode($response, true);
 echo "<pre>";
+$data = json_decode($response, true);
 print_r($data);
 echo "</pre>";
+
 ?>
