2d5845cc7c74d80b9a9b9a231630910567671c5e
ramywthyq-byte <ramywthyq@gmail.com>
2026-06-27 01:32:08 +0300

    Update download.php

--- Boot/download.php
+++ Boot/download.php
@@ -23,6 +23,7 @@
 
     curl_setopt($ch,CURLOPT_URL,$url);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
     $reselt = curl_exec($ch);
     curl_close($ch);
