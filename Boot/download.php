b75066a536d04a6a04237f657b80f77ecd6168bb
ramywthyq-byte <ramywthyq@gmail.com>
2026-06-27 01:08:46 +0300

    Update download.php

--- Boot/download.php
+++ Boot/download.php
@@ -1,18 +1,39 @@
 <?php
+
     header("Content-Type: application/json");
 
-    if(!isset($_GET['url'])){
+     if(!isset($_GET['url'])){
         echo json_encode([
             "status" => false,
             "message" => "no url"
         ],JSON_PRETTY_PRINT);
         exit;
     }
-    $url = $_GET['url'];
+    $url = trim($_GET['url']);
     
-    echo json_encode([
-        "status" => true,
-        "message" => $url
-    ],JSON_PRETTY_PRINT);
+    if(!filter_var($url,FILTER_VALIDATE_URL)){
+        echo json_encode([
+            "status" => false,
+            "messame" => "invaild url"
+        ],JSON_PRETTY_PRINT);
+        exit;
+    }
+    
+    $ch = curl_init();
+
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+
+    $reselt = curl_exec($ch);
+    curl_close($ch);
+    echo $reselt;
+
+
+
+
+
+
+   
+
 
 ?>
